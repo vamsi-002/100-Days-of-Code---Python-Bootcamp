329de4f50ed298f95c8d0f7ee0e1061ab7fa88e8
Vamsidhar <vamsidhar2589@gmail.com>
2024-08-13 23:38:42 +0530

    complete Day17

---
+++ Day 17-Quiz Game/data.py
@@ -0,0 +1,23 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
+    {"text": "The Eiffel Tower can be 15 cm taller during the summer.", "answer": "True"},
+    {"text": "Bananas grow on trees.", "answer": "False"},  # Bananas grow on large herbaceous plants, not trees.
+    {"text": "There are more stars in the universe than grains of sand on all the Earth's beaches.", "answer": "True"},
+    {"text": "The unicorn is the national animal of Scotland.", "answer": "True"},
+    {"text": "Humans share 50% of their DNA with bananas.", "answer": "True"},
+    {"text": "Goldfish only have a memory of three seconds.", "answer": "False"},  # They can remember things for months.
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "A group of crows is called a murder.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"}  # It's a myth; it is not visible from space.
+]
