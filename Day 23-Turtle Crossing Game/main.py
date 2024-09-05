9c2c2c3e16972abcfe72cff92454bab1cc6d4617
Vamsidhar <vamsidhar2589@gmail.com>
2024-09-05 19:23:59 +0530

    complete Day23

---
+++ Day 23-Turtle Crossing Game/main.py
@@ -0,0 +1,36 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+user = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(user.go_up, 'Up')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(user) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+    
+    if user.at_finish_line():
+        user.goto_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
